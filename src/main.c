ff104e3f8cc5c14925be426b16ecad691a8ec456
Andrew Jessup <jessup@jessup-macbookpro2.roam.corp.google.com>
2015-09-02 17:58:51 -0700

    (BROKEN) Uptates main to set light status from interrupt, and scale output from ADC and write to the screen. Currently borked.

--- src/main.c
+++ src/main.c
@@ -1,17 +1,3 @@
-// This is your second program to run on the LaunchPad
-// You will debug this program as your Lab 4
-// If both switches SW1 and SW2 are pressed, the LED should be blue
-// If just SW1 switch is pressed,            the LED should be red
-// If just SW2 switch is pressed,            the LED should be green
-// If neither SW1 or SW2 is pressed,         the LED should be off
-
-// 0.Documentation Section 
-// main.c
-// Runs on LM4F120 or TM4C123
-// Lab4_IO, Inputs from PF4,PF0, output to PF3,PF2,PF1 (LED)
-// Authors: Daniel Valvano, Jonathan Valvano and Ramesh Yerraballi
-// Date: December 28, 2014
-
 // LaunchPad built-in hardware
 // SW1 left switch is negative logic PF4 on the Launchpad
 // SW2 right switch is negative logic PF0 on the Launchpad
@@ -22,6 +8,8 @@
 #include "gpio.h"
 #include "tm4c123gh6pm.h"
 #include "nokia.h"
+#include "scrooge.h"
+#include <math.h>
 
 // 2. Declarations Section
 //   Global Variables
@@ -29,75 +17,75 @@ unsigned long SW1, SW1_prev,SW2;  // input from PF4,PF0
 unsigned long Out;      // outputs to PF3,PF2,PF1 (multicolor LED)
 
 
+
 // 3. Subroutines Section
 // MAIN: Mandatory for a C Program to be executable
 int main(void){
   //unsigned short mode=0;
+  char screenbuffer[84][6];
 
   //TExaS_Init(SW_PIN_PF40,LED_PIN_PF321); 
-  // TExaS_Init initializes the real board grader for lab 4
   PortF_Init();        // Call initialization of port PF4, PF3, PF2, PF1, PF0    
-  
-  // Init interrupt
-  
-  
+  PortE_Init();        // Init ADC and PE3
+    
   Nokia_InitDisplay();
+
+  // Show scrooge while loading...
+  //DisableInterupts();
+  //Nokia_WriteImg(scroogeImg);
+  
 
   // Enable interrupts
   EnableInterupts();
+  
+    while(1){
+        DisableInterupts();
+        short color = pf4_push_count % 8;
+        switch(color){
+            case 0:
+                GPIO_PORTF_DATA_R = 0x02;
+                break;
+            case 1:
+                GPIO_PORTF_DATA_R = 0x04;
+                break;
+            case 2:
+                GPIO_PORTF_DATA_R = 0x08;
+                break;
+            case 3:
+                GPIO_PORTF_DATA_R = 0x0A;
+                break;
+            case 4:
+                GPIO_PORTF_DATA_R = 0x0C;
+                break;
+            case 5:
+                GPIO_PORTF_DATA_R = 0x0E;
+                break;
+            case 6:
+                GPIO_PORTF_DATA_R = 0x06;
+                break;
+            case 7:
+            default:
+                GPIO_PORTF_DATA_R = 0x00;
+                break;
+        }
 
-  while(1){
-	short color = pf4_push_count % 8;
-	switch(color){
-		case 0:
-			GPIO_PORTF_DATA_R = 0x02;
-			break;
-		case 1:
-			GPIO_PORTF_DATA_R = 0x04;
-			break;
-		case 2:
-			GPIO_PORTF_DATA_R = 0x08;
-			break;
-		case 3:
-			GPIO_PORTF_DATA_R = 0x0A;
-			break;
-		case 4:
-			GPIO_PORTF_DATA_R = 0x0C;
-			break;
-		case 5:
-			GPIO_PORTF_DATA_R = 0x0E;
-			break;
-		case 6:
-			GPIO_PORTF_DATA_R = 0x06;
-			break;
-		case 7:
-		default:
-			GPIO_PORTF_DATA_R = 0x00;
-			break;
-	}
-	WaitForInterrupt();
-  }
-
-
-
-
-	// while(1){
-	//     SW1 = GPIO_PORTF_DATA_R&0x01;     // read PF0 into SW1
-	//     SW2 = GPIO_PORTF_DATA_R&0x10;     // read PF4 into SW2
-	//     if(SW1&&SW2){                     // both pressed
-	//       GPIO_PORTF_DATA_R = 0x04;       // LED is blue
-	//     } else{
-	//       if(SW1&&(!SW2)){                // just SW1 pressed
-	//         GPIO_PORTF_DATA_R = 0x02;     // LED is red
-	//       } else{
-	//         if((!SW1)&&SW2){              // just SW2 pressed
-	//           GPIO_PORTF_DATA_R = 0x08;   // LED is green
-	//         }else{                        // neither switch
-	//           GPIO_PORTF_DATA_R = 0x00;   // LED is off
-	//         }
-	//       }
-	//     }
-	//   }
+        // Read in audio samples, and if we have something write it to the screen
+        if(currentRead.readCount == SAMPLE_LENGTH) {
+            // Max read of ADC is 12bits (4024), bit-shift 7 to max of 36
+            for(int i=0;i<80;i++){
+                unsigned short scaledSample = currentRead.samples[i] >> 7;
+                for(int j=0;j<scaledSample;j++){
+                    short multiple = (j / 6);
+                    screenbuffer[i][multiple] |= (0x1 << (j-(multiple*6)));
+                }
+            }
+            currentRead.readCount = 0;
+            //Nokia_ClearScreen();
+            Nokia_WriteImg(screenbuffer);
+        }
+        EnableInterupts();
+        WaitForInterrupt();
+    }
 }
 
 

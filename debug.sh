0201028adce6e02ab21199c7d6067f38f3960a15
Andrew Jessup <jessup@jessup-macbookpro2.roam.corp.google.com>
2015-09-02 18:01:17 -0700

    Clean up cruft

--- debug.sh
+++ debug.sh
@@ -1,13 +1,7 @@
 export OPENOCD=/usr/local/Cellar/open-ocd/HEAD/bin/openocd
 export BRDPATH=/usr/local/Cellar/open-ocd/HEAD/share/openocd/scripts/board/ek-tm4c123gxl.cfg
 
-#$OPENOCD -f $BRDPATH &
-#arm-none-eabi-gdb -ex 'target extended-remote :3333;'
-
-# arm-none-eabi-gdb -ex 'target extended-remote | $OPENOCD -f $BRDPATH -c "gdb_port pipe; log_output openocd.log"; monitor reset halt; load; monitor reset init;' build/a.out
-
 arm-none-eabi-gdb -ex 'target extended-remote | $OPENOCD -f $BRDPATH -c "gdb_port pipe; log_output openocd.log"; monitor reset halt; load;' build/a.out
-
 
 # http://processors.wiki.ti.com/index.php/Stellaris_Launchpad_with_OpenOCD_and_Linux
 # (gdb) monitor reset halt

69be5a76d4b3b696af52700279486582c57b11ae
Andrew Jessup <ajessup@gmail.com>
2015-11-28 11:32:51 -0800

    Adds third party heap allocation library, + tests (which segfault on my OS X box)

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,4 +1,10 @@
 set -e
 set -o pipefail # Fail if any process fails
+
+echo "== Testing Fast Fourier Transform Lib =="
 gcc -I../src ../src/fft.c test_fft.c -o test_ffc
 ./test_ffc
+
+echo "== Testing dynamic memory allocation lib =="
+gcc -I../src ../src/heap.c HeapTestMain.c -o test_heap
+./test_heap 

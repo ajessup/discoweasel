f64cf4c45f3265deee9b55cb08dba8ef3184a83f
Andrew Jessup <ajessup@gmail.com>
2015-12-06 18:01:19 -0800

    Bugfixes

--- src/nokia.h
+++ src/nokia.h
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 
-#define NOKIA_SCREEN_V_SEGMENTS 6;
+#define NOKIA_SCREEN_V_SEGMENTS 6
+#define NOKIA_SCREEN_V_SEGMENTS_HEIGHT 8
 
 void TM4C123_SSI_Init(void);
 void Nokia_InitDisplay(void);
